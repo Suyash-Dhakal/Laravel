9d9719bed2cb877c9593767c3c5256823ad64de0
Suyash Dhakal <suyash.dhakal@icloud.com>
2024-06-21 16:37:57 +0545

    First Laravel Project (used laravel directives like @include,@extends,@yeild,etc)

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.mainlayout')
+@section('content')
+<h1>About Page</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae tenetur voluptatibus quasi error praesentium dignissimos ipsa odio? Doloribus fugit a perferendis beatae, est accusamus sed impedit itaque magnam assumenda veniam modi dolore veritatis, amet rerum. Est in commodi molestiae dolore, iure sapiente! Eum voluptate sapiente perferendis dolorem hic soluta eligendi ducimus, corporis dolore possimus non ipsum cupiditate cum modi veniam nisi at voluptatibus eos, fugit quia minima. In nesciunt, voluptates atque quisquam sunt vel minus consequatur unde repudiandae? Aliquam facilis, quam eius ex temporibus commodi obcaecati! Tenetur repellendus quam eos, animi in aut distinctio aperiam quisquam voluptate incidunt quas facere.</p>
+@endsection
+@section('title')
+About
+@endsection
---
+++ resources/views/layout/mainlayout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suyash - @yield('title','Website')</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!-- <div class="test"><h1 class="test">Suyash</h1></div> -->
+
+<div id="box">
+<header id="head">
+<h1>Suyash Dhakal</h1>
+</header>
+<nav><a href="/">Home</a>  |  <a href="/About">About</a>  |  <a href="/Post">Post</a></nav>
+<main>
+    <article>
+        @yield('content')
+         </article>
+    <aside>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/About">About</a></li>
+            <li><a href="/Post">Post</a></li>
+        </ul>
+    </aside>
+</main>
+<footer id="foot">
+suyash-dhakal@copyright 2024.
+</footer>
+</div>
+   
+</body>
+</html>
